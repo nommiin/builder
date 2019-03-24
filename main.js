a81372d941745715d7c8960a370d970b7b440812
Torin Freimiller <torinfrei@gmail.com>
2019-03-23 23:14:37 -0500

    Version number changed

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ $gmedit["gml.Project"].current
                 preferences.setMenu(preferences.menuMain);
                 Builder.Save();
             });
-            preferences.addText(Builder.Settings, "builder v0.2 - nommiin")
+            preferences.addText(Builder.Settings, "builder v0.3 - nommiin")
 
             // Hook into preferences menu
             let buildMain = preferences.buildMain;
